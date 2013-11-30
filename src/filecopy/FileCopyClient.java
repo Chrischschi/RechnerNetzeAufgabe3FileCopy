22e5a60fd9965e6ab06265e9eec9669e29278b94
Chrischschi <Christian.Schirin@haw-hamburg.de>
2013-11-30 20:25:16 +0100

    Commit nach der heutigen Saros-sitzung
    FileCopyClient.java:
    berechnung des timeouts implementiert (computeTimeoutValue(long)),
    implementierung von timeOutTask(long)
    bearteitung von runFileCopyClient, hauptsächlich kommentare
    einführung von konstanten DELAY und INFLUENCE_COEFFICIENT,
    einführung von instanzvariablen (unter "Variables added by us")
    anpassung des Konstruktors
    FileCopyClient(String,String,String,String,String)
    
    Zu erledigende arbeiten stehen in den kommentaren bei runFileCopyClient()

--- src/filecopy/FileCopyClient.java
+++ src/filecopy/FileCopyClient.java
@@ -1,13 +1,20 @@
 package filecopy;
 
 /* FileCopyClient.java
- Version 0.1 - Muss erg�nzt werden!!
+ Version 0.1 - Muss erg�nzt werden!!
  Praktikum 3 Rechnernetze BAI4 HAW Hamburg
  Autoren:
  */
 
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.LinkedList;
 
 public class FileCopyClient extends Thread {
 
@@ -32,26 +39,59 @@ public class FileCopyClient extends Thread {
   // -------- Variables
   // current default timeout in nanoseconds
   private long timeoutValue = 100000000L;
-
+  
+  // -------- Constants added by us
+  
+  private long DELAY = 1; //This variable is used in the sendThreads to simulate a RTT increased by the value of delay in miliseconds
+  
+  private static final float INFLUENCE_COEFFICIENT = 0.1f; //the "x" value of the RTT calculation
+  
+  // -------- Variables added by us
+  
+  private InetAddress serverAdress;
+  
+  private DatagramSocket clientSocket;
+  
+  private LinkedList<FCpacket> sendBuffer; //A list of the packets of the current window (Use a ArrayList?)
+  
+  private long estimatedRTT; //The RoundTripTime used to calculate the timeoutValue
+  							 //EstimatedRTT = (1-x)*EstimatedRTT + x*SampleRTT
+  
+  private long deviation; //Variable to incorporate RTT variance
+  
+  private int numberOfTimeouts = 0;
   // ... ToDo
 
 
   // Constructor
   public FileCopyClient(String serverArg, String sourcePathArg,
-    String destPathArg, String windowSizeArg, String errorRateArg) {
+    String destPathArg, String windowSizeArg, String errorRateArg) throws UnknownHostException {
     servername = serverArg;
+    serverAdress = InetAddress.getByName(servername);
     sourcePath = sourcePathArg;
     destPath = destPathArg;
     windowSize = Integer.parseInt(windowSizeArg);
     serverErrorRate = Long.parseLong(errorRateArg);
-
+    sendBuffer = new LinkedList<FCpacket>();
   }
 
-  public void runFileCopyClient() {
-
+  public void runFileCopyClient() throws IOException {
+	  clientSocket = new DatagramSocket();
+	  //1.Buffer füllen -> Lese x Pakete à 8Byte SeqNumber und 1000Byte Daten ein.
+	  //2a.Sende KontrollPaket
+	  //(2b.Server stellt sich dem kontrollpaket entsprechend ein)
+	  //3.Sende die Daten-Pakete (Alle auf einmal? -> ack verpasst?(vlt eigener Thread zum warten auf antwort))
+	  //4.Warte auf Antwort
+	  //5.Antwort gekommen -> Markiere das Packet des ack als empfangen
+	  //6.Betrachte 1. element der Liste. Entferne es, wenn es als empfangen markiert ist.
+	  //7.wiederhole 5. bis das 1. element der liste nicht markiert ist.
+	  //8.Fülle den Buffer, bis dieser eine größe von windowSize erreicht hat oder alle Daten der datei eingelesen wurden und sende die neuen Pakete.
+	  //9.gehe zu 3 wenn der buffer nicht leer ist
       // ToDo!!
-
-
+	  //WillBeDone
+	  
+	  
+	  
   }
 
   /**
@@ -78,7 +118,17 @@ public class FileCopyClient extends Thread {
    * Implementation specific task performed at timeout
    */
   public void timeoutTask(long seqNum) {
-  // ToDo
+	  numberOfTimeouts++;
+	  int currentIndexOfPacket = new Long(seqNum-sendBuffer.get(0).getSeqNum()).intValue();
+	  FCpacket packet = sendBuffer.get(currentIndexOfPacket);
+	  startTimer(packet);
+	  sendThread send = new sendThread(toDatagramPacket(packet));
+	  send.run();
+  }
+  
+  private DatagramPacket toDatagramPacket(FCpacket packet){
+	  return new DatagramPacket(packet.getData(),
+              packet.getLen(), serverAdress, SERVER_PORT);
   }
 
 
@@ -87,8 +137,14 @@ public class FileCopyClient extends Thread {
    * Computes the current timeout value (in nanoseconds)
    */
   public void computeTimeoutValue(long sampleRTT) {
-
-  // ToDo
+	  estimatedRTT = new Float((1-INFLUENCE_COEFFICIENT)*estimatedRTT+INFLUENCE_COEFFICIENT*sampleRTT).longValue();
+	  deviation = new Float((1-INFLUENCE_COEFFICIENT)*deviation+INFLUENCE_COEFFICIENT*(sampleRTT-estimatedRTT)).longValue();
+	  timeoutValue = estimatedRTT + 4*deviation;
+	  testOut("computeTimeoutValue"+
+			  "\nsRTT: " + sampleRTT +
+			  "\neRTT: " + estimatedRTT +
+			  "\nDeviation: " + deviation +
+			  "\ntimeoutValue");
   }
 
 
@@ -108,7 +164,31 @@ public class FileCopyClient extends Thread {
     }
     return new FCpacket(0, sendData, sendData.length);
   }
+  
+  /**
+   * Create thread to send a packet to the Server
+   * 
+   */
+  private class sendThread extends Thread {
+	    /* Thread for sending of one ACK-Packet with propagation delay */
+	    DatagramPacket packet;
 
+	    public sendThread(DatagramPacket packet) {
+	      this.packet = packet;
+	    }
+
+	    public void run() {
+	      try {
+	        Thread.sleep(DELAY);
+	        clientSocket.send(packet);
+	      } catch (Exception e) {
+	        e.printStackTrace();
+	        System.err.println("Unexspected Error! " + e.toString());
+	        System.exit(-1);
+	      }
+	    }
+	  }
+  
   public void testOut(String out) {
     if (TEST_OUTPUT_MODE) {
       System.err.printf("%,d %s: %s\n", System.nanoTime(), Thread
